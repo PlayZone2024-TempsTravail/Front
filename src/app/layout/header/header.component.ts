80f0beda2f780564917cd32b0a61323ef020d489
Maudoux Eva <eva.maudoux@gmail.com>
2024-11-18 11:33:28 +0100

    "eva"

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -9,15 +9,6 @@ import { Link } from '../../core/models/link';
 export class HeaderComponent {
   links: Link[] = [
     { title: 'Accueil', url: "/" },
-    { title: 'L\'adoption', url: "/adoption",
-      children: [
-          { title: 'Nos animaux', url: '/adoption/nos-animaux' },
-          { title: 'Les adoptés', url: '/adoption/animaux-adoptes' },
-      ], isVisibile: false
-    },
-    { title : 'Qui sommes-nous ?', url: "/a-propos", },
-    { title: 'Nous rendre visite', url: '/contactez-nous'},
-    
   ];
 
   toggleIsVisible(link: Link) {
