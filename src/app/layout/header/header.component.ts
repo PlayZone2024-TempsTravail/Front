53c20fdcf98d1af950b165b348d302d800a0c111
Maudoux Eva <eva.maudoux@gmail.com>
2024-11-18 13:57:07 +0100

    modif navbar eva

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -7,16 +7,34 @@ import { Link } from '../../core/models/link';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  links: Link[] = [
-    { title: 'Accueil', url: "/" },
-  ];
+    links: Link[] = [
+        { title: 'Accueil', url: "/" },
+        { title: 'Pointages', url: "/pointages",
+            children: [
+                { title: 'Calendrier', url: '/pointages/calendrier' },
+                { title: 'Pointages employés', url: '/pointages/pointages-employes' },
+            ], isVisibile: false
+        },
+        { title: 'Projet', url: '/projet',
+            children: [
+                {title: 'Budget', url: '/projet/budget'},
+                {title: 'Rapport', url: '/projet/rapport'},
+                {title: 'Projet', url: '/projet/projet'},
+            ], isVisibile: false
+        },
+        {title: 'Gestion', url: '/gestion',
+            children: [
+                {title: 'Utilisateurs', url: '/gestion/utilisateurs'},
+                {title: 'Projets', url: '/gestion/projets'},
+            ], isVisibile: false
+        }
+    ];
 
   toggleIsVisible(link: Link) {
-    for (let l of this.links) {
-        if (l !== link) {
-            l.isVisibile = false;
-        }
-    }
-    link.isVisibile = !link.isVisibile;
-}
+      for (let l of this.links) {
+          if (l !== link) {l.isVisibile = false;
+          }
+      }
+      link.isVisibile = !link.isVisibile;
+  }
 }
