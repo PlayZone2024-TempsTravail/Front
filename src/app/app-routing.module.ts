69d13692dec33eafcd1bbde03b517fa0f2e7bc9f
Maudoux Eva <eva.maudoux@gmail.com>
2024-11-26 16:48:10 +0100

    rectif user-list et user-form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './features/auth/pages/login/login.component';
-import { authenticatedGuard, unauthenticatedGuard } from './shared/guards/authenticated.guard';
-import { CalendarComponent } from './features/time-tracking/components/calendar/calendar.component';
+import {authenticatedGuard, unauthenticatedGuard} from './shared/guards/authenticated.guard';
+import {LoginComponent} from './features/auth/pages/login/login.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/pointage', pathMatch: 'full' },
-    { path: 'pointage', component: CalendarComponent, canActivate: [unauthenticatedGuard]},
+    { path: 'pointage', loadChildren: () => import('./features/time-tracking/time-tracking.module').then(m => m.TimeTrackingModule), canActivate: [unauthenticatedGuard]},
     { path: 'projet', loadChildren: () => import('./features/projects/projects.module').then(m => m.ProjectsModule), canActivate: [unauthenticatedGuard] },
     { path: 'equipe', loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule), canActivate: [unauthenticatedGuard] },
     { path: 'configuration', loadChildren: () => import('./features/configuration/configuration.module').then(m => m.ConfigurationModule), canActivate: [unauthenticatedGuard] },
     { path: 'login', component: LoginComponent, canActivate: [authenticatedGuard] },
-];
+    ];
 
 
 @NgModule({
--- src/app/features/users/components/user-list/user-list.component.ts
+++ src/app/features/users/components/user-list/user-list.component.ts
@@ -4,9 +4,9 @@ import {ActivatedRoute} from '@angular/router';
 import {UserForm} from '../../models/user.form.model';
 
 @Component({
-  selector: 'app-user-list',
-  templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.scss'
+    selector: 'app-user-list',
+    templateUrl: './user-list.component.html',
+    styleUrl: './user-list.component.scss'
 })
 export class UserListComponent implements OnInit {
 
