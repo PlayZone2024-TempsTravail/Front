f1c2dbbbf1f55cc6dace5df4a54b78daab2dfcaa
Louis Delleur <louis.delleur@hotmail.com>
2024-12-09 16:41:05 +0100

    feature config visuel et debut connection API

--- src/app/features/configuration/configuration.module.ts
+++ src/app/features/configuration/configuration.module.ts
@@ -3,15 +3,33 @@ import { CommonModule } from '@angular/common';
 
 import { ConfigurationRoutingModule } from './configuration-routing.module';
 import { ConfigurationComponent } from './configuration/configuration.component';
+import {Button} from "primeng/button";
+import {ChipModule} from 'primeng/chip';
+import {PrimeTemplate} from 'primeng/api';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {TableModule} from 'primeng/table';
+import {FloatLabelModule} from 'primeng/floatlabel';
+import {SharedModule} from '../../shared/shared.module';
+import {DividerModule} from 'primeng/divider';
+import {InputNumberModule} from 'primeng/inputnumber';
 
 
 @NgModule({
   declarations: [
     ConfigurationComponent
   ],
-  imports: [
-    CommonModule,
-    ConfigurationRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        ConfigurationRoutingModule,
+        Button,
+        ChipModule,
+        PrimeTemplate,
+        RadioButtonModule,
+        TableModule,
+        FloatLabelModule,
+        SharedModule,
+        DividerModule,
+        InputNumberModule
+    ]
 })
 export class ConfigurationModule { }
--- src/app/features/configuration/configuration/configuration.component.ts
+++ src/app/features/configuration/configuration/configuration.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ConfigurationService} from '../services/configuration.service';
+import {Category, ConfigurationList} from '../models/configuration.model';
 
 @Component({
   selector: 'app-configuration',
   templateUrl: './configuration.component.html',
   styleUrl: './configuration.component.scss'
 })
-export class ConfigurationComponent {
+export class ConfigurationComponent implements OnInit {
 
+    configs:  ConfigurationList[] = []
+    categories: Category[] = []
+
+    //forms
+
+
+    constructor(private configurationServices:ConfigurationService) {
+    }
+
+    ngOnInit(): void {
+        this.configurationServices.getConfiguration().subscribe(c => {
+            this.configs = c;
+        })
+        this.configurationServices.getCategory().subscribe(c => {
+            this.categories = c;
+        })
+    }
+
+
+
+    modification(event:number){
+
+    }
+
+
+    protected readonly parseFloat = parseFloat;
 }
