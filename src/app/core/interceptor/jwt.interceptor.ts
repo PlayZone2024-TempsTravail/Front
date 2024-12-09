57f0228c671b94d3c7ef3cf7667550fb401772bc
Maudoux Eva <eva.maudoux@gmail.com>
2024-12-09 10:29:17 +0100

    merge and fix errors

--- src/app/core/interceptor/jwt.interceptor.ts
+++ src/app/core/interceptor/jwt.interceptor.ts
@@ -5,21 +5,21 @@ import {inject} from "@angular/core";
 import {catchError, throwError} from 'rxjs';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  const authService: AuthService = inject(AuthService);
-  const userToken: UserTokenDtoModel | undefined = authService.currentUser;
+    const authService: AuthService = inject(AuthService);
+    const userToken: UserTokenDtoModel | undefined = authService.currentUser;
 
-  const clonedRequest = userToken && userToken.token
-    ? req.clone({
-        headers: req.headers.set('Authorization', `Bearer ${userToken.token}`)
-      })
-    : req;
+    const clonedRequest = userToken && userToken.token
+        ? req.clone({
+            headers: req.headers.set('Authorization', `Bearer ${userToken.token}`)
+        })
+        : req;
 
-  return next(clonedRequest).pipe(
-    catchError(err => {
-      if (err.status === 401 || err.status === 403) {
-        authService.logout();
-      }
-      return throwError(() => err);
-    })
-  );
+    return next(clonedRequest).pipe(
+        catchError(err => {
+            if (err.status === 401) {
+                authService.logout();
+            }
+            return throwError(() => err);
+        })
+    );
 };
