139500edf2d36718a6d8b98a14f3ed805586e30c
Astor Whiletrue <patignylouis@gmail.com>
2024-12-11 15:14:33 +0100

    Fix Page Créer Projet

--- src/app/features/projects/forms/project-creation.form.ts
+++ src/app/features/projects/forms/project-creation.form.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+﻿import { Injectable } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Injectable({
@@ -10,13 +10,13 @@ export class ProjectCreationForm {
     createForm(): FormGroup {
         return this.fb.group({
             name: ['', [Validators.required]],
-            organismeId: [[Validators.required]],
-            chargerDeProjetId: [[Validators.required]],
-            montantBudget: [0,[Validators.required, Validators.min(0)]],
-            dateDebutProjet: [[Validators.required]],
-            dateFinProjet: [[Validators.required]],
+            organismeId: ['',[Validators.required]],
+            chargerDeProjetId: ["",[Validators.required]],
+            montantBudget: [0,[Validators.required, Validators.min(0.1)]],
+            dateDebutProjet: [null,[Validators.required]],
+            dateFinProjet: [null,[Validators.required]],
             isActive: [true, [Validators.required]],
-            color: [[Validators.required]],
+            color: ["#45def1", [Validators.required]],
         });
     }
 }
