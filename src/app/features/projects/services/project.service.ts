82ec16b066041841689efac38567914659eb400b
Astor Whiletrue <patignylouis@gmail.com>
2024-12-11 11:38:07 +0100

    bonjour la branche dev

--- src/app/features/projects/services/project.service.ts
+++ src/app/features/projects/services/project.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Depense, Project, LibeleWithName} from '../models/project.model';
+import {Project, LibeleWithName} from '../models/project.model';
 import { map } from 'rxjs/operators';
 
 @Injectable({
